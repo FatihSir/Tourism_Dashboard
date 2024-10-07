243702897337429eddc7730c3960a37da365ad33
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-07 22:30:58 +0300

    Sidebar improvements

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar/>
+
             <Routes>
               <Route path="/" element={<Dashboard />} />
             </Routes>
